21a4a7e85012615fd5aaf83b7a4aef92b35ac8c3
Hal Canary <halcanary@gmail.com>
2020-07-17 09:58:01 -0400

    html->markdown

---
+++ src/turndown.js
@@ -0,0 +1,11 @@
+var TurndownService = require('turndown');
+var fs = require("fs");
+
+const readstdin = () => (fs.readFileSync(0).toString());
+
+var turndownService = new TurndownService();
+turndownService.keep(['img', 'table', 'div', 'span', 'input', 'form', 'script'])
+var html = readstdin();
+var markdown = turndownService.turndown(html);
+process.stdout.write(markdown);
+process.stdout.write("\n");
