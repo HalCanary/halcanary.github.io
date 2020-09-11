896cde3069f093f3367fc9292fb4b600e4663eae
Hal Canary <halcanary@gmail.com>
2020-09-11 08:58:18 -0400

    refactor blog

--- src/makeblog.py
+++ src/makeblog.py
@@ -3,32 +3,17 @@
 
 import sys, os, math, subprocess, re, datetime, tempfile, hashlib, shutil
 
-base_dir = 'vv'
-url_base = 'https://halcanary.org'
-
-relurlfixer = re.compile('^{}/(.*)$'.format(base_dir))
+BASE_DIR = 'vv'
+URL_BASE = 'https://halcanary.org'
 
 MARKDOWN_CMD = ['cmark']
 if float(re.sub(r'cmark (\d+\.\d+)\..*', r'\1', subprocess.check_output(['cmark', '--version']), flags=re.S)) >= 0.29:
     MARKDOWN_CMD = ['cmark', '--unsafe']
 
-first_year = '1997'
 current_year = '{:04d}'.format(datetime.date.today().year)
 
-thisdir  = os.path.dirname(__file__)
-with open(thisdir + '/style.css') as f:
+with open(os.path.dirname(__file__) + '/style.css') as f:
     style = re.sub(r'(^|:)  *', r'\1', f.read(), flags=re.M).strip()
-
-footer = """
-<!-- END CONTENT -->
-
-      </div>
-      <hr class="black">
-
-      <p>(<a href="../../../../archives/">back</a>)</p>
-  </body>
-</html>
-"""
 
 header = """<!DOCTYPE html>
 <html lang="en">
@@ -41,42 +26,33 @@ header = """<!DOCTYPE html>
 {style}
 </style>
 </head>
-  <!-- Copyright {years} Hal Canary. ALL RIGHTS RESERVED. -->
+<!-- Copyright {years} Hal Canary. ALL RIGHTS RESERVED. -->
+<body>
 """
 
 top_posts_header = """
-  <body>
-      <h1>Voder-Vocoder</h1>
-      <div class="byline centered">The Log of Hal Canary</div>
+<h1>Voder-Vocoder</h1>
+<div class="byline centered">The Log of Hal Canary</div>
 
-      <script>
-        function search_hco() {
-          document.getElementById("search-link-halcanary-org").style.display = "none";
-          document.getElementById("search-halcanary-org").style.display = "block";
-          return false;
-        }
-      </script>
-      <div id="search-link-halcanary-org">
-        <div class="rightside"><a href="" onclick="return search_hco();">search</a></div>
+<div class="postbox">
+  <div id="search-link-halcanary-org">
+    <div class="rightside"><a href="" onclick="document.getElementById('search-link-halcanary-org').style.display='none'; document.getElementById('search-halcanary-org').style.display='block'; return false;">search</a></div>
+  </div>
+  <div id="search-halcanary-org" style="display:none;">
+    <form method="get" action="https://www.google.com/search">
+      <div class="rightside">
+        <input name="domains" value="halcanary.org" type="hidden" />
+        <input name="sitesearch" value="halcanary.org" type="hidden" />
+        <input id="search" name="q" size="30" maxlength="255" />
+        <input value="Search" type="submit" />
       </div>
-      <div id="search-halcanary-org" style="display:none;">
-        <form method="get" action="https://www.google.com/search">
-          <div class="rightside">
-            <input name="domains" value="halcanary.org" type="hidden" />
-            <input name="sitesearch" value="halcanary.org" type="hidden" />
-            <input id="search" name="q" size="30" maxlength="255" />
-            <input value="Search" type="submit" />
-          </div>
-        </form>
-      </div>
-      <div class="postbox">&nbsp;</div>
-      <div>
+    </form>
+  </div>
+</div>
 """
 top_posts_footer = """
-      </div>
-      <div class="centered"><a href="archives/">MORE</a></div>
-      <div>&nbsp;</div>
-  </body>
+<div class="centered"><a href="archives/">MORE</a></div>
+</body>
 </html>
 """
 
@@ -127,19 +103,6 @@ class UpdatingFile(object):
         else:
             sys.stderr.write(*args, **kwargs)
 
-post_header = """
-  <body>
-      <h1 class="blogtitle">{title}</h1>
-{summarypart}
-      <div class="byline">By Hal Canary,
-        {date}
-        (<a href="{permalink}">link</a>)
-{category_links}        </div>
-      <!-- SRC= {src} -->
-      <div class="content">
-
-<!-- BEGIN CONTENT -->
-"""
 
 class Post(object):
     @staticmethod
@@ -154,6 +117,30 @@ class Post(object):
         s = subproc.stdout.read()
         subproc.wait()
         return s
+
+    @staticmethod
+    def write(o, post_info, level):
+        # keys = [ 'src', 'permalinkpart', 'title', 'summary', 'date', 'category_links', 'post' ]
+        post_template = """
+<article class="postbox">
+  <header>
+    <!-- SRC= {src} -->
+    <h{level} class="blogtitle">{title}</h{level}>
+    <p>{summary}</p>
+    <div class="byline">
+      <div>by: Hal Canary</div>
+      <div>posted: {date}</div>
+      <div>link: <a href="/{permalinkpart}">{URL_BASE}/{permalinkpart}</a></div>
+      <div>{category_links}</div>
+    </div>
+  </header>
+  <div class="content">
+{post}
+  </div>
+</article>
+"""
+        o.write(post_template.format(level=level, URL_BASE=URL_BASE, **post_info))
+
     def __init__(self, path):
         # Required:
         #    TITLE
@@ -163,32 +150,25 @@ class Post(object):
         #    SUMMARY
         #    CATEGORIES
         #    MODE
-        # Not suggested, but used:
-        #    PERMALINK
-        #    PERMALINKPART
         # Everything else is ignored
         if not os.path.isfile(path):
-            raise Exception(path+" is not a file")
-        self.source_path = path
+            raise Exception(path + " is not a file")
+
         self.src = os.path.basename(path)
-
-        self.current_year = '{:04d}'.format(datetime.date.today().year)
-
-        self.title = None
+        self.category_links = ''
+        self.category_list = []
+        self.summary = ''
         self.date = None
-        self.postid = None
-
-        self.permalink = None
+        self.day = None
+        self.month = None
         self.permalinkpart = None
+        self.post = None
+        self.title = None
+        self.year = None
 
-        self.summary = None
-        self.summarypart = ''
-        self.categories = ''
-        self.mode = 'default'
-        self.style = style
-
-        #self.values = {}
-        with open(self.source_path,'r') as f:
+        postid = None
+        mode = None
+        with open(path,'r') as f:
             while True:
                 sline = f.readline().strip()
                 if sline == '':
@@ -205,180 +185,86 @@ class Post(object):
                 elif key == 'DATE':
                     self.date = value
                 elif key == 'POSTID':
-                    self.postid = value
+                    postid = value
                 elif key == 'SUMMARY':
                     self.summary = value
                 elif key == 'CATEGORIES':
-                    self.categories = value
+                    self.category_list = filter(len, value.split(';'))
                 elif key == 'MODE':
-                    self.mode = value
+                    mode = value
                 elif key != 'COPYRIGHT':
                     sys.stderr.write('%s: %r=%r\n' % (path, key, value))
 
-            if self.date is None:
-                raise Exception('DATE missing ({})'.format(self.source_path))
-            if self.postid is None:
-                raise Exception('POSTID missing ({})'.format(self.source_path))
-            if self.title is None:
-                raise Exception('TITLE missing ({})'.format(self.source_path))
-
-            if self.summary is not None:
-                self.summarypart = (
-                    '\n      <p>'
-                    + self.summary + '</p>\n')
+            if mode.lower() == 'markdown':
+                self.post = Post.markdown(f).rstrip()
             else:
-                self.summarypart = ''
+                self.post = f.read().rstrip()
 
-            timestamp = datetime.datetime.strptime(
-                self.date.split()[0],'%Y-%m-%d').date()
-            self.year = '{:04d}'.format(timestamp.year)
-            self.month = '{:02d}'.format(timestamp.month)
-            self.day = '{:02d}'.format(timestamp.day)
+        if self.date is None:
+            raise Exception('DATE missing ({})'.format(path))
+        if postid is None:
+            raise Exception('POSTID missing ({})'.format(path))
+        if self.title is None:
+            raise Exception('TITLE missing ({})'.format(path))
 
-            self.permalinkpart = '{}/{}/{}/{}/{}/'.format(
-                    base_dir,self.year,self.month,self.day,
-                    self.postid)
+        timestamp = datetime.datetime.strptime(self.date.split()[0], '%Y-%m-%d').date()
+        self.year  = '{:04d}'.format(timestamp.year)
+        self.month = '{:02d}'.format(timestamp.month)
+        self.day   = '{:02d}'.format(timestamp.day)
 
-            self.permalink = url_base + '/' + self.permalinkpart
-            self.target_file = self.permalinkpart + 'index.html'
+        category_fmt = '<a href="/{1}/category/{0}/" class="categorylink">#{0}</a>'
+        self.category_links = '; '.join(category_fmt.format(cat, BASE_DIR) for cat in self.category_list)
+        self.permalinkpart = '{}/{}/{}/{}/{}/'.format(BASE_DIR, self.year, self.month, self.day, postid)
 
-            self.category_list = filter(len, self.categories.split(';'))
-            self.category_links = ''
-            if (len(self.category_list) > 0):
-                cfmt = ('<a href="../../../../category/{0}/" ' +
-                    'class="categorylink">#{0}</a>')
-                self.category_links = ';'.join(cfmt.format(cat)
-                                           for cat in self.category_list)
-                self.category_links = '    <br>{}\n'.format(
-                    self.category_links)
+    def write_to_file(self, output_path):
+        o = UpdatingFile(output_path)
+        o.write(header.format(title='Voder-Vocoder: ' + self.title, style=style, years=self.year + '-' + current_year))
+        Post.write(o, vars(self), 1)
+        o.write('\n<p>(<a href="/vv/archives/">back</a>)</p>\n</body>\n</html>\n')
+        if o.close():
+            sys.stdout.write('modified %s\n' % o.path)
 
-            o = UpdatingFile(self.target_file)
-            o.write(header.format(title='Voder-Vocoder: ' + self.title, style=style, years=self.year + '-' + self.current_year))
-            o.write(post_header.format(**self.__dict__))
-
-            if self.mode.lower() == 'markdown':
-                self.post = Post.markdown(f)
-            else:
-                self.post = f.read()
-            o.write(self.post)
-            o.write(footer)
-            if o.close():
-                sys.stdout.write('modified %s\n' % self.target_file)
-
-recent_post = """
-<div class="postbox">
-      <h2 class="blogtitle">{title}</h2>
-{summarypart}
-      <div class="byline">By Hal Canary,
-        {date}
-        (<a href="{link}">link</a>)
-        {category_links}
-      </div>
-      <!-- SRC={src} -->
-      <div class="content">
-{post}
-      </div>
-</div>
-
-"""
 class RecentPosts(object):
     def __init__(self):
-        self.loc = '{}/index.html'.format(base_dir)
-        self.o = UpdatingFile(self.loc)
-        self.o.write(header.format(style=style, title='Voder-Vocoder', years='1997-' + current_year))
-        self.o.write(top_posts_header)
-    def close(self):
-        self.o.write(top_posts_footer)
-        if self.o.close():
-            sys.stdout.write('modified %s\n' % self.loc)
-    def addPost(self,post):
-        relurlfixer = re.compile('^{}/(.*)$'.format(base_dir))
-        relurl = relurlfixer.match(post.permalinkpart).groups()[0]
-        self.o.write(
-            recent_post.format(
-                title=post.title,
-                summarypart=post.summarypart,
-                date=post.date,
-                link=relurl,
-                category_links=post.category_links,
-                post=post.post,
-                src=post.src))
-
-
-
-class Index(object):
-    @staticmethod
-    def gethash(arg):
-        return hash('{}/{}/index.html'.format(base_dir, '/'.join(arg)))
-    def __hash__(self):
-        return hash(self.loc)
-    def __init__(self, arg, name=''):
         self.posts = []
-        '''
-        arg is in the format
-            ['category','?*']
-            ['????','??']
-            ['????']
-        '''
-        if len(name) > 0:
-            self.name = '<br>'+name
-        else:
-            self.name = ''
-        self.upwards = '../' * len(arg)
-        self.loc = '{}/{}/index.html'.format(base_dir, '/'.join(arg))
-        self.addyear = False
-
-    def addPost(self, post, addyear = False):
-        d = vars(post)
-        keys = [ 'permalinkpart', 'year', 'title', 'summary', 'date' ]
-        self.posts += [{k: d.get(k) for k in keys}]
-        self.addyear = addyear
-
+    def addPost(self, post):
+        self.posts.append(post)
     def close(self):
-        o = UpdatingFile(self.loc)
-        o.write(header.format(style=style, title='Voder-Vocoder Archive', years='1997-' + current_year))
-        archive_header = """
-  <body>
-    <h1><a href="/vv/" class="hiddenlink">Voder-Vocoder</a> Archive{name}</h1>
-    <div class="content">
-<!-- BEGIN CONTENT -->
-"""
-        o.write(archive_header.format(name=self.name))
-        lastyear = None
+        o = UpdatingFile('{}/index.html'.format(BASE_DIR))
+        o.write(header.format(style=style, title='Voder-Vocoder', years='1997-' + current_year))
+        o.write(top_posts_header)
         for post in self.posts:
-            relurlfixer = re.compile('^{}/(.*)$'.format(base_dir))
-            short = relurlfixer.match(post['permalinkpart']).groups()[0]
-            relurl = self.upwards + short
-            assert post['year'] is not None
-            if lastyear is not None and lastyear != post['year']:
-                lastyear = None
-                o.write('    </ul>\n')
-            if lastyear is None:
-                lastyear = post['year']
-                if self.addyear:
-                    o.write('<h2>{year}</h2>'.format(year=post['year']))
-                o.write('    <ul>\n')
-
-            o.write(
-                '<li>\n  <a href="{relurl}">{title}</a>\n  {date}\n'.format(
-                    relurl=relurl,title=post['title'],date=post['date']))
-            if post['summary'] is not None:
-                o.write('  <br>{}\n'.format(post['summary']))
-            o.write('</li>\n')
-
-        archive_footer = """
-<!-- END CONTENT -->
-    </div>
-    <hr class="black">
-    <p class="centered"><a href="../">UP</a></p>
-  </body>
-</html>
-"""
-        if lastyear is not None:
-            o.write('    </ul>\n')
-        o.write(archive_footer)
+            Post.write(o, vars(post), 2)
+        o.write(top_posts_footer)
         if o.close():
-            sys.stdout.write('modified %s\n' % self.loc)
+            sys.stdout.write('modified %s\n' % o.path)
+
+def addPost(indices, pathlist, name, post):
+    info = {k: vars(post)[k] for k in ['permalinkpart', 'year', 'title', 'summary', 'date']}
+    _, posts = indices.setdefault(pathlist, (name, {}))
+    posts.setdefault(post.year, []).append(info)
+
+def write_indices(indices):
+    for pathlist, (name, posts) in indices.items():
+        loc = '{}/{}/index.html'.format(BASE_DIR, '/'.join(pathlist))
+        o = UpdatingFile(loc)
+        o.write(header.format(style=style, title='Voder-Vocoder Archive', years='1997-' + current_year))
+        o.write('<h1><a href="/vv/" class="hiddenlink">Voder-Vocoder</a> Archive<div>{name}</div></h1>'.format(name=name))
+        for year in sorted(posts.keys(), reverse=True):
+            year_posts = posts[year]
+            if len(posts) > 1:
+                o.write('<h2>{year}</h2>\n'.format(year=year))
+            o.write('<ul>\n')
+            for post in year_posts:
+                fmt = '<li>\n  <a href="/{permalinkpart}">{title}</a>\n  {date}\n  <div>{summary}</div>\n</li>\n'
+                o.write(fmt.format(**post))
+            o.write('</ul>\n\n')
+
+        o.write('<hr class="black">\n' +
+                '<p class="centered"><a href="../">UP</a></p>\n' +
+                '</body>\n</html>\n')
+        if o.close():
+            sys.stdout.write('modified %s\n' % loc)
 
 def listdir(path, reverse=False):
     assert os.path.isdir(path)
@@ -386,15 +272,6 @@ def listdir(path, reverse=False):
     files.sort(reverse=reverse)
     for fn in files:
         yield os.path.join(path, fn)
-
-def findIndex(indices, arg, name=''):
-    h = Index.gethash(arg)
-    if h in indices:
-        return indices[h]
-    else:
-        n = Index(arg, name)
-        indices[h] = n
-        return n
 
 if __name__ == '__main__':
     src_dir = os.path.abspath(os.path.dirname(__file__))
@@ -411,19 +288,18 @@ if __name__ == '__main__':
             continue
         if os.path.isfile(path):
             post = Post(path)
+            post.write_to_file(post.permalinkpart + 'index.html')
+
             if i < 10:
                 recentPosts.addPost(post)
-            findIndex(indices, ['archives'], '').addPost(post, True)
-            findIndex(indices, [post.year], post.year).addPost(post)
-            findIndex(indices, [post.year, post.month],
-                      post.year+'-'+post.month).addPost(post)
-            findIndex(indices, [post.year, post.month, post.day],
-                      post.year+'-'+post.month+'-'+post.day).addPost(post)
+            ymd = [post.year, post.month, post.day]
+            addPost(indices, tuple(ymd[0:1]),       '{}'.format(*ymd), post)
+            addPost(indices, tuple(ymd[0:2]),    '{}-{}'.format(*ymd), post)
+            addPost(indices, tuple(ymd[0:3]), '{}-{}-{}'.format(*ymd), post)
+            addPost(indices, ('archives',), '', post)
             for cat in post.category_list:
-                findIndex(indices, ['category',cat],'#'+cat).addPost(post)
-
+                addPost(indices, ('category', cat), '#' + cat, post)
         else:
             sys.stderr.write("???? %s\n" % path)
     recentPosts.close()
-    for index in indices.itervalues():
-        index.close()
+    write_indices(indices)
